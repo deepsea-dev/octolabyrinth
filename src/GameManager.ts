4c10e6f2e3176624e9660674cc4ff2d90afeadb4
Bradley Bellairs <bwbellairs@hellomouse.net>
2022-11-12 14:14:20 +0000

    Added game manager and models

---
+++ src/GameManager.ts
@@ -0,0 +1,20 @@
+import { randomBytes } from 'crypto';
+import Game from './Game';
+
+class GameManager {
+  games: Record<string, Game> = {};
+  constructor() {
+    this.games = {};
+  }
+
+  addGame() {
+    const id = randomBytes(2).toString('hex');
+    this.games[id] = new Game(id);
+  }
+
+  getGame(id: string) {
+    return;
+  }
+}
+
+export default GameManager;
