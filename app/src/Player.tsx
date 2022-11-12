3e04ed0c8fe2262f41f47b78bc229f604d3eda20
harry <harrybest800@gmail.com>
2022-11-12 17:15:42 +0000

    Add name submission

--- app/src/Player.tsx
+++ app/src/Player.tsx
@@ -1,7 +1,36 @@
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { queryApi } from './wrappedFetch';
 
 export const Player = () => {
   const { gameId } = useParams();
+
+  const [nameIsSet, setNameIsSet] = useState(false);
+  const [playerName, setPlayerName] = useState<string>('');
+
+  const [playerId, setPlayerId] = useState<string | null>(null);
+
+  const submitName = () => {
+    if (playerName.length == 0) return;
+    setNameIsSet(true);
+    queryApi<{player_id: string}>(`/api/${gameId}/join`, 'POST', {nickname: playerName}).then((res) => setPlayerId(res.player_id));
+  };
+
+  if (!nameIsSet) {
+    return (
+      <div>
+        <div>Enter your name: </div>
+        <input onChange={e => setPlayerName(e.target.value)} value={playerName}/>
+        <button onClick={submitName}>submit</button>
+      </div>
+    );
+  }
+
+  if (playerId == null) {
+    return (
+      <div>Joining the game...</div>
+    );
+  }
 
   return (
     <div>PLAYER of game {gameId}</div>
