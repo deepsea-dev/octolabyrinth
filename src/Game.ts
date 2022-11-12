219cc0e68f588db7454423bed319456f3d4dd4a9
Bradley Bellairs <bwbellairs@outlook.com>
2022-11-12 17:13:16 +0000

    Allow players to join games

--- src/Game.ts
+++ src/Game.ts
@@ -1,8 +1,22 @@
+import { randomBytes } from 'crypto';
+import Player from './Player';
+
 class Game {
   id: string;
+  players: Record<string, Player>;
   constructor(id: string) {
     this.id = id;
-    
+    this.players = {};
+  }
+
+  addPlayer(id: string, nickname: string) {
+    let playerID;
+    do {
+      playerID = randomBytes(2).toString('hex');
+    } while (this.players[playerID] !== undefined);
+
+    this.players[playerID] = new Player(playerID, nickname);
+    return playerID;
   }
 }
 
--- src/controllers/rootController/rootController.ts
+++ src/controllers/rootController/rootController.ts
@@ -6,12 +6,13 @@ import express from 'express';
 import { CreateGameResponse } from '../../models/CreateGameResponse';
 import { HelloMessageResponse } from '../../models/helloMessageResponse';
 import GameManager from '../../GameManager';
+import { GameJoinResponse } from '../../models/GameJoinResponse';
 
 const game_manager = new GameManager();
 
 export const init = (app: express.Application): void => {
   app.get('/api', async (req, res: express.Response<HelloMessageResponse>) => {
-    res.status(200).json({message: `Hello: ${req.headers['user-agent']}`});
+    res.status(200).json({ message: `Hello: ${req.headers['user-agent']}` });
   });
   app.get('/api/create_game', async (req, res: express.Response<CreateGameResponse>) => {
     const id = game_manager.addGame();
@@ -19,5 +20,14 @@ export const init = (app: express.Application): void => {
   });
   app.get('/api/get_game', async (req, res) => {
     return;
+  });
+  app.post('/api/:id/join', async (req, res: express.Response<GameJoinResponse>) => {
+    if (!req.body.nickname) return res.sendStatus(400);
+    try {
+      const player_id = game_manager.addPlayer(req.params.id, req.body.nickname);
+      return res.status(200).json({ player_id });
+    } catch {
+      return res.sendStatus(400);
+    }
   });
 };
