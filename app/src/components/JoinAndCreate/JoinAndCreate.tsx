b705c066b0ab4e1ab71b46ca1bc15c586aa542d5
harry <harrybest800@gmail.com>
2022-11-12 14:11:11 +0000

    Add query on create game

--- app/src/components/JoinAndCreate/JoinAndCreate.tsx
+++ app/src/components/JoinAndCreate/JoinAndCreate.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import { CreateGameResponse } from '../../models/CreateGameResponse';
+import { queryApi } from '../../wrappedFetch';
+
+type GameCode = string
 
 export const JoinAndCreate: React.FC = () => {
+  const createGame = () => {
+    queryApi<CreateGameResponse>('/api/create_game').then((res) => {
+      setGameCode(res.id);
+    }).catch((err) => {});
+
+  };
+
+  const [gameCode, setGameCode] = useState<GameCode>('');
+
   return (
     <JoinAndCreateContainer>
       <JoinContainer>
-        <input placeholder='enter game code'></input>
+        <input placeholder='enter game code' onChange={(e) => setGameCode(e.target.value)} value={gameCode}></input>
         <ActionButton>Join</ActionButton>
       </JoinContainer>
-      <ActionButton>Create</ActionButton>
+      <ActionButton onClick={createGame}>Create</ActionButton>
     </JoinAndCreateContainer>
   );
 };
