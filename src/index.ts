f9a98b1989028d07c8d1438874d558d77c03b644
harry <harrybest800@gmail.com>
2022-11-13 00:43:36 +0000

    remove dirname

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('site'));
 
   app.get('*', function(req, res) {
-    res.sendFile('index.html', {root: path.join(__dirname, 'site')});
+    res.sendFile('index.html', {root: 'site'});
   });
 }
 
